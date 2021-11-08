40f171a8439f0a39ac8a6b9d9941f59db28ef640
MichaelMtz <michaelmtz@mtzdesigns.com>
2021-11-08 11:01:01 -0700

    Fix Endpoint api

--- src/_functions/snowreport-api.js
+++ src/_functions/snowreport-api.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.handler = async function(event, context, callback) {
   const {target,src} = event.queryStringParameters;
-  let endpoint = (src === 'resort') ? 'list' : 'resort';
+  let endpoint = (src !== 'resort') ? 'list' : 'resort';     
   const url = `https://feeds.snocountry.net/proof-of-concept/headless-snow-report-${endpoint}.php?target=${target}`;
   const response = await axios.get(url);
   callback(null, {
