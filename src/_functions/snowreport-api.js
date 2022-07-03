ab815367a177c221d3203dee8479fdb7103ed2ed
MichaelMtz <michaelmtz@mtzdesigns.com>
2022-07-03 17:11:45 -0600

    Debug snowreport-api

--- src/_functions/snowreport-api.js
+++ src/_functions/snowreport-api.js
@@ -6,7 +6,7 @@ exports.handler = async function(event, context, callback) {
   const prod = `https://feeds.snocountry.net/proof-of-concept/headless-snow-report-${endpoint}.php?target=${target}`;
   const dev = `http://localhost/sno/snoCountryHeadless/snow-reports/headless-snow-report-${endpoint}.php?target=${target}`;
   const url = (window.location.hostname === 'localhost') ? dev: prod; 
-  
+  console.log(`--snowreport-api: (${url})`);
   const response = await axios.get(url);
   callback(null, {
     statusCode: 200,
