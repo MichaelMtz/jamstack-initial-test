7334afd8bbd7fc57651df74900ad9ea526a4245f
MichaelMtz <michaelmtz@mtzdesigns.com>
2022-11-09 07:28:12 -0700

    Region listing tweak

--- src/_functions/snowreport-api.js
+++ src/_functions/snowreport-api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 exports.handler = async function(event, context, callback) {
   const {target,src} = event.queryStringParameters;
   const endpoint = (src !== 'resort') ? 'list' : 'resort';     
-  const url = `https://feeds.snocountry.net/proof-of-concept/headless-snow-report-${endpoint}.php?target=${target}`;
+  const url = `https://feeds.snocountry.net/proof-of-concept/headless-snow-report-${endpoint}.php?target=${target}&src=${src}`;
 
   const response = await axios.get(url);
   callback(null, {
