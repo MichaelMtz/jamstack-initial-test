5ae1c41f0b1ef9000193328e7ead1fbf72454c07
MichaelMtz <michaelmtz@mtzdesigns.com>
2022-12-31 16:04:05 -0700

    Widget to Netlify fn initial attempt

---
+++ src/_functions/widget.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+exports.handler = async function(event, context, callback) {
+  const {code, state, type, region, pettabs, size, color } = event.queryStringParameters;
+  const url = `www.snocountry.com/widget/widget_resort.php?code=${code}&state=${state}&type=${type}&region=${region}&pettabs=${pettabs}&size=${size}&color=${color}`;
+
+  const response = await axios.get(url);
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: response.data
+  });
+};
