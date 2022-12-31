0b4517f63f673ff533dfbc2985f5356a12fff153
MichaelMtz <michaelmtz@mtzdesigns.com>
2022-12-31 14:54:53 -0700

    Walk back Netlify splat redirect test

--- src/assets/js/home-page.js
+++ src/assets/js/home-page.js
@@ -256,8 +256,9 @@ document.addEventListener('DOMContentLoaded',()=> {
               <h6 class="remove-bottom cabin"><span class="small ucase">${iterPost.author}</span><br><strong>${iterPost.title}</strong></h6>
           </div><!-- end deals-content -->
           <div class="deals-desc"> 
-              <!-- <a href="news-post/?postID=${iterPost.id}" class="button readbtn dealClick" > -->
-              <a href="news-post/${iterPost.permalink}?postID=${iterPost.id}" class="button readbtn dealClick" >
+              <!-- <a href="news-post/${iterPost.permalink}?postID=${iterPost.id}" class="button readbtn dealClick" > -->
+              
+              <a href="news-post/?postID=${iterPost.id}" class="button readbtn dealClick" >
               Read...
               </a>
           </div><!-- end desc -->
