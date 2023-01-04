21c63f0ad4340af270cb3492ce66dfc5328d02b2
MichaelMtz <michaelmtz@mtzdesigns.com>
2023-01-04 08:28:12 -0700

    Netlify Fn fix

--- src/_functions/news-list-api.js
+++ src/_functions/news-list-api.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 exports.handler = async function(event, context, callback) {
   console.log('event:',event);
-  const { postID } = event.queryStringParameters;
-  const url = `https://www.snow-country.com/resorts/api-easy-blog-list.php?notPostID=${postID}`;
+  const { notPostID } = event.queryStringParameters;
+  const url = `https://www.snow-country.com/resorts/api-easy-blog-list.php?notPostID=${notPostID}`;
   const response = await axios.get(url);
   callback(null, {
     statusCode: 200,
