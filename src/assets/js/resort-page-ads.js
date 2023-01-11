6645ce2f19f6ee48b62fa8790e3484e56fea7025
MichaelMtz <michaelmtz@mtzdesigns.com>
2023-01-11 13:11:38 -0700

    Pepsi ROTW on resort page

--- src/assets/js/resort-page-ads.js
+++ src/assets/js/resort-page-ads.js
@@ -294,4 +294,34 @@ document.addEventListener('DOMContentLoaded',()=> {
 
   }
 
+  if (resort_id === '303022') {
+    waitForElement('#resort-name').then((elTarget) => {
+      const html = `
+        <div id="pepsi" class="pepsi">
+        
+            <div class="pepsi-content">
+                <div class="pepsi-header">
+                    <div class="pepsi-logo-container">   
+                        <img class="logo-telluride" src="assets/images/ads/pepsi/telluride/TSR_bold-100.png" alt="Telluride Ski Resort">
+                        <img src="assets/images/ads/pepsi/pepsi-zero-sugar-logo.png" alt="Pepsi" class="logo-pepsi">
+                    </div>
+                    <div class="pepsi-copy">Resort of the Week</div>
+                    <audio controls id="myaudio">
+                      
+                       <source src="assets/audio/pepsi/Telluride-ROTW.mp3" type="audio/mpeg">
+                    
+                    </audio>
+                </div>
+                <div class="pepsi-video">
+                  <iframe width="640" height="360" src="https://www.youtube.com/embed/jxuJt6T6FsM?autoplay=1&mute=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="border-radius:3px;"></iframe>
+                </div>
+            </div>    
+        
+
+        </div><!-- end pepsi --> 
+      `;
+      elTarget.insertAdjacentHTML('beforebegin',html);
+    }).catch( () => { console.log('Error waiting for checkForResortAds:');});
+  }
+
 });
