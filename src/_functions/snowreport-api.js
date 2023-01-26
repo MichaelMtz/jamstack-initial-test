1a8706bb1994d626b2c6213cca6d36f6a9181c45
MichaelMtz <michaelmtz@mtzdesigns.com>
2023-01-25 20:10:50 -0700

    Tweak Fn: snowreport-api for XC

--- src/_functions/snowreport-api.js
+++ src/_functions/snowreport-api.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 
 exports.handler = async function(event, context, callback) {
-  const {target,src} = event.queryStringParameters;
+  const {target,src,type} = event.queryStringParameters;
   const endpoint = (src !== 'resort') ? 'list' : 'resort';     
-  const url = `https://feeds.snocountry.net/proof-of-concept/headless-snow-report-${endpoint}.php?target=${target}&src=${src}`;
+  const url = `https://feeds.snocountry.net/proof-of-concept/headless-snow-report-${endpoint}.php?target=${target}&src=${src}&type=${type}`;
 
   const response = await axios.get(url);
   callback(null, {
