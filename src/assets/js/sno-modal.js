745d7d0e7ce3acb6b4548d9e44bd28ea7ac7f975
MichaelMtz <michaelmtz@mtzdesigns.com>
2023-11-13 13:40:14 -0700

    Add crypto for Safari and auto focus

--- src/assets/js/sno-modal.js
+++ src/assets/js/sno-modal.js
@@ -204,7 +204,13 @@ const setupSnoActions = () => {
 };
 
 
-
+if (! (crypto.randomUUID instanceof Function)) {
+  crypto.randomUUID = function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  };
+}
 
 document.addEventListener('DOMContentLoaded',()=> {
   _log('snoModal processing...');
