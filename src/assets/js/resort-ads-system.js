1bfc021247d5cefa647c7a1ffdbce93a58d7367d
Michael Martinez <michaelmtz@mtzdesigns.com>
2025-10-23 17:02:19 -0600

    Fix resort ad, part deux

--- src/assets/js/resort-ads-system.js
+++ src/assets/js/resort-ads-system.js
@@ -2,6 +2,34 @@
 const t = function (e) {return "font-weight:bold;font-size:1em;font-family:arial,helvitica,sans-serif;color:" + e;};
 const _log = function (text, param, color = 'DeepSkyBlue') {  console.log(`%cs%cn%co%cw %c==> ${text}`, t("#ADD8E6"), t("#87CEEB"), t("#87CEFA"), t("#00BFFF"), `font-size:11px; font-weight:500; color:${color}; padding:3px 50px 3px 3px; width:100%;`, param);};
 
+const waitForElement = (selector) => {
+  return new Promise(function (resolve, reject) {
+    const element = document.querySelector(selector);
+
+    if (element) {
+      resolve(element);
+      return;
+    }
+
+    const observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        const nodes = Array.from(mutation.addedNodes);
+        for (const node of nodes) {
+          if (node.matches && node.matches(selector)) {
+            observer.disconnect();
+            resolve(node);
+            return;
+          }
+        }
+      });
+    });
+
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+  });
+};
 /**
  * Resort Ads System - Dynamic Advertisement Management
  * 
